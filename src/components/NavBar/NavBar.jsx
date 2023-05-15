f495b6a621f413c1bf391c784b1532ba2138721c
Vilem-McKael <vilemckael@gmail.com>
2023-05-15 01:55:26 -0700

    updates welcome page, adds link to logo to redirect to welcome page

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -14,7 +14,7 @@ export default function NavBar({user, updateUser, currentCollective}) {
         <div className='nav flex flex-col justify-center w-[20vw] h-[15vh]'>
         
           <div className="flex flex-col items-center justify-center text-center mt-[1.5vh] h-[6vh] mb-[1.5vh]">
-            <img src='https://i.imgur.com/eyfjME1.png' className='h-[10vh] mt-[3vh]'/>
+            <Link to='/' className='Link2'><img src='https://i.imgur.com/eyfjME1.png' className='h-[10vh] mt-[3vh]'/></Link>
           </div>
           <div className="flex flex-row justify-evenly items-center bg-gradient-to-b from-blue-500 to-indigo-500 h-[6vh]">
               <Link to='/collectives/search' className='Link mt-[1vmin]'><i className='icon flaticon-search-1 text-[calc(12px+1vh)]'></i></Link>
--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -3,9 +3,15 @@ import React from 'react'
 export default function WelcomePage({user}) {
   return (
     <>
-        <div>WelcomePage</div>
-        <p>username: {user.username}</p>
-        <p>id: {user.user_id}</p>
+        <div className='flex w-[80vw] h-[96vh] justify-center items-center'>
+            <div className='flex flex-col items-center w-[40vw] h-[40vh] mb-[10vh] p-[4vmin] bg-gradient-to-b from-sky-600 via-indigo-700 to-blue-700 rounded-[10px] ring-8 ring-indigo-400'>
+                <img src='https://i.imgur.com/eyfjME1.png' className='h-[16vh]'/>
+                <h1 className='text-[30px] text-center text-[#F5F5F5] mt-[-4vh]'>Welcome to Catalyst</h1>
+                <h2 className='text-[18px] text-center text-indigo-200 mt-[1vh]'>...well, what're you waiting for!</h2>
+                <h2 className='text-[18px] text-center text-indigo-200 mt-[1vh]'>Get started by searching for a community by clicking the Search icon in the upper left-hand corner.</h2>
+                <h2 className='text-[18px] text-center text-indigo-200 mt-[1vh]'>Can't find exactly what you're looking for? Create your own by clicking the 'Add Collective' icon!</h2>
+            </div>
+        </div>
     </>
   )
 }
