4f63b7d6e9297eca343d8b9ab258c98d9c9dc0c3
Vilem-McKael <vilemckael@gmail.com>
2023-05-15 09:26:25 -0700

    makes welcome page and new collective page more responsive

--- src/pages/NewCollectivePage/NewCollectivePage.jsx
+++ src/pages/NewCollectivePage/NewCollectivePage.jsx
@@ -37,11 +37,11 @@ export default function NewCollectivePage({collectives, updateCollectives}) {
     return (
         <div className='flex w-[80vw] h-[96vh] justify-center items-center'>
             <div className='flex flex-col justify-center items-center w-[40vw] h-[40vh] mb-[10vh] p-[4vmin] bg-gradient-to-b from-sky-600 via-indigo-700 to-blue-700 rounded-[10px] ring-8 ring-indigo-400'>
-                <h1 className='text-[30px] text-[#F5F5F5]'>Establish a new Collective</h1>
-                <h2 className='text-[18px] text-indigo-200'>What kind of space will you create?</h2>
+                <h1 className='text-[2.5vmin] text-[#F5F5F5] text-center'>Establish a new Collective</h1>
+                <h2 className='text-[1.5vmin] text-indigo-200'>What kind of space will you create?</h2>
                 <form autoComplete="off" onSubmit={handleSubmit} className='flex flex-col items-center'>
-                    <input type="text" name="name" value={collectiveData.name} placeholder='Collective Name:' onChange={handleChange} className='mt-[2vh] w-[30vw] text-[20px] text-stone-700' required /><br/>
-                    <textarea type="text" name="description" value={collectiveData.description} placeholder='In a Few Words, Describe Your New Collective ...' onChange={handleChange} className='w-[30vw] h-[10vh] text-stone-700 p-[.5vmin] pl-[1vmin] rounded-[5px]' required /><br/>
+                    <input type="text" name="name" value={collectiveData.name} placeholder='Collective Name:' onChange={handleChange} className='mt-[2vh] w-[30vw] text-[1.75vmin] text-stone-700' required /><br/>
+                    <textarea type="text" name="description" value={collectiveData.description} placeholder='In a Few Words, Describe Your New Collective ...' onChange={handleChange} className='w-[30vw] h-[10vh] text-stone-700 text-[1.5vmin] p-[.5vmin] pl-[1vmin] rounded-[5px]' required /><br/>
                     <button type="submit" className='text-[#F5F5F5] text-[20px] bg-indigo-700'>create</button>
                 </form>
             </div>
